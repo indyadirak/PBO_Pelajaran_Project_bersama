a1aa27913c8b34f81e4789b8646ff2bf6d37a807
muchamadkurniawan <kernel.journal@itats.ac.id>
2022-11-29 10:50:29 +0700

    add data_barang

--- src/Main.java
+++ src/Main.java
@@ -1,27 +1,35 @@
-import data.data_kasir;
-
+import data.data_barang;
+import entity.entity_barang2;
 public class Main {
     public static void main(String[] args){
         System.out.println("Project PBO 2022");
 
         //input data kasir
-        data_kasir database_kasir = new data_kasir();
-        database_kasir.create("kurniawan",155,"123");
-        database_kasir.create("aan",156,"123");
-        database_kasir.read(); // viev database kasir
+//        data_kasir database_kasir = new data_kasir();
+//        database_kasir.create("kurniawan",155,"123");
+//        database_kasir.create("aan",156,"123");
+//        database_kasir.read(); // viev database kasir
+//
+//        // cek method update
+//        database_kasir.update_pass(111,"111"); //pengujian salah
+//        database_kasir.update_pass(155,"111"); //pengujian benar
+//        database_kasir.read();
+//
+//        //cek method delete
+//        database_kasir.delete(111); //pengujian salah
+//        database_kasir.delete(155); //pengujian benar
+//        database_kasir.read();
+//
+//        //tes login
+//        int nip1 = database_kasir.login(156,"111");
+//        int nip2 = database_kasir.login(156,"123");
 
-        // cek method update
-        database_kasir.update_pass(111,"111"); //pengujian salah
-        database_kasir.update_pass(155,"111"); //pengujian benar
-        database_kasir.read();
-
-        //cek method delete
-        database_kasir.delete(111); //pengujian salah
-        database_kasir.delete(155); //pengujian benar
-        database_kasir.read();
-
-        //tes login
-        int nip1 = database_kasir.login(156,"111");
-        int nip2 = database_kasir.login(156,"123");
+        //data barang
+        data_barang DBbarang = new data_barang();
+        DBbarang.create(1,"pepsodent",7000,1);
+        DBbarang.create(2,"ciptadent",5000,12);
+        DBbarang.view();
+        System.out.println("==========overload======");
+        DBbarang.view(1);
     }
 }
--- src/data/data_barang.java
+++ src/data/data_barang.java
@@ -1,13 +1,30 @@
 package data;
-import entity.entity_barang;
-
+import entity.entity_barang2;
 import java.util.ArrayList;
 
-public class data_barang extends entity_barang{
-    private ArrayList<entity_barang> dataBarang = new ArrayList<>();
+public class data_barang {
+    private ArrayList <entity_barang2> data_barang;
 
-    public data_barang(int kodeBarang, String nama, int harga, int stok) {
-        super(kodeBarang, nama, harga, stok);
-//        this.dataBarang.add()
+    public data_barang()
+    {
+        this.data_barang = new ArrayList<>();
+    }
+
+    public void create(int kode, String nama, int harga, int stok)
+    {
+        this.data_barang.add(new entity_barang2(kode,nama,harga,stok));
+    }
+
+    public void view()
+    {
+        for (int i=0;i<this.data_barang.size();i++)
+        {
+            this.data_barang.get(i).cetak();
+        }
+    }
+
+    public void view(int index)
+    {
+        this.data_barang.get(index).cetak();
     }
 }
